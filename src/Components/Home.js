7df5316f55bc3d640ed5033d24f874edee817876
Ali UĞUR <39213991+alithethird@users.noreply.github.com>
2023-03-16 16:56:14 +0300

    dünyanın en güzel dişçisi

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,14 +2,15 @@ import * as React from 'react';
 import { Link } from 'react-router-dom';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import { pink } from '@mui/material/colors';
 
 export default function Home() {
 
     return (
         <>
-            <h1>Ana Sayfa</h1>
+            <h1>Dünyanın en güzel dişçisinin hasta listesi</h1>
             <Stack spacing={2} direction="column">
-                <Link to="/Form" style={{paddingLeft: 13, textDecoration: 'none'}}><Button variant="contained">Hasta Ekle</Button></Link>
+                <Link to="/Form" style={{paddingLeft: 13, textDecoration: 'none' }}><Button variant="contained">Hasta Ekle</Button></Link>
 
                 <Link to="/Listings" style={{paddingLeft: 13, textDecoration: 'none'}}><Button variant="contained">Listeyi görüntüle</Button>  </Link>
             </Stack></>
